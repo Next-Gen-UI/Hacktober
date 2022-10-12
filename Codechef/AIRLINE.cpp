dab04db149b2ca9d26326371bb186892ec060175
Aayushprasad000 <57568262+Aayushprasad000@users.noreply.github.com>
2022-10-12 14:51:23 +0530

    comment checked

--- Codechef/AIRLINE.cpp
+++ Codechef/AIRLINE.cpp
@@ -1,13 +1,7 @@
-//Ramnit here baby
+
 #include<bits/stdc++.h>
 using namespace std;
-
-// typedef long long lolo;
 #define fasterDaddy ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-// #define all(v) v.begin,v.end()
-// #define puba push_back
-// #define F .first
-// #define S .second
 
 int main() {
 	fasterDaddy;
@@ -35,9 +29,7 @@ int main() {
               continue;
             }
         }
-        // else{
             cout<<"NO\n";
-        // }
     }
 	return 0;
 }
